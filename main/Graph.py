c27b7dcb3e20f06a8983ab9e29951f3975cb5855
Hugo <theemeraldman7@gmail.com>
2025-06-05 10:57:25 +0200

    comment + fix

--- main/Graph.py
+++ main/Graph.py
@@ -6,22 +6,33 @@ class Graph:
 
     def __init__(self, grid):
         self.nodes = []
+        # first, we create the nodes (by giving the type of graph construction)
         self.CreateNode(grid, 'NEIGHBOR')
+        #then, we create the edges of the graph
         self.CreateEdges(grid)
 
 
+    # function to create the nodes of the graph (not the edges)
+    # grid = 2D Array of the grid (input) giving example : [[0,0,0],[1,0,0]]
+    # type = type of contruction for graph
     def CreateNode(self, grid, type="NEIGHBOR"):
+        # global list to stock all visited position
         pos_already_visited = []
         for y in range(len(grid)):
             for x in range(len(grid[y])):
+                # if already visited or is nothing (0)
                 if ((x,y) in pos_already_visited or (grid[y][x] == 0)):
                     continue
                 pixel_found = [(x,y)]
                 pixel_value = [(x,y)]
+                # if we find a new node, we are going to find all pixels associated based on type of search
+                # the functions are recursive. 
+                #note: pixel_found passed through REFERENCE (no return to the fonctions)
                 if type == "NEIGHBOR":
                     self.CreateNodeWithNeighbors(grid, [x,y], pixel_found, pos_already_visited)
                 elif type == "COLOR":
                     self.CreateNodeWithColor(grid, [x,y], pixel_found, pixel_value, pos_already_visited)
+                # end of the search, we add the pixels to the node created
                 node = Node(pixel_found) 
                 self.nodes.append(node)
         print(pos_already_visited)
@@ -30,21 +41,29 @@ class Graph:
     # current_pos[0] -> x
     # current_pos[1] -> y
     # pixel found = position of pixels (local to this node)
-    # pos alreay visited = position of pixels already visited (global au parcours de la grid)
+    # pos alreay visited = position of pixels already visited (global to the search) VALUE OF RETURN, PASSED THROUGH REFERENCE
+    # pixel_value = color of first nodes
     def CreateNodeWithColor(self, grid, current_pos, pixel_found, pixel_value, pos_already_visited):
+         # double for, checking around the current pixel
          for y in range (-1, 2):
             for x in range (-1, 2):
                 if (x == 0 and y == 0):
                     continue
+                
                 # position in grid
                 pos_tested = (current_pos[0] + x, current_pos[1] + y)
                 if ((pos_tested[0] < 0 or pos_tested[0] >= len(grid[0])) or (pos_tested[1] < 0 or pos_tested[1] >= len(grid))):
                     continue
+               
                 # value in grid
                 actual_value = grid[pos_tested[1]][pos_tested[0]]
+
+                # if value is a new value 
                 if (actual_value == pixel_value and pos_tested not in pixel_found):
+                    # we add to pixel_found (passed throug hreference)
                     pixel_found.append(pos_tested)
                     pos_already_visited.append(pos_tested)
+                    # recursive call to check position
                     self.CreateNodeWithColor(grid, pos_tested, pixel_found, pos_already_visited)
 
 
@@ -54,20 +73,27 @@ class Graph:
     # current_pos[1] -> y
     # pixel found = position of pixels (local to this node)
     # pos alreay visited = position of pixels already visited (global au parcours de la grid)
+    # SAME AS CreateNodeWithColor,check for this function for more comments
     def CreateNodeWithNeighbors(self, grid, current_pos, pixel_found, pos_already_visited):
         for y in range (-1, 2):
             for x in range (-1, 2):
                 if (x == 0 and y == 0):
                     continue
+
                 # position in grid
                 pos_tested = (current_pos[0] + x, current_pos[1] + y)
                 if ((pos_tested[0] < 0 or pos_tested[0] >= len(grid[0])) or (pos_tested[1] < 0 or pos_tested[1] >= len(grid))):
                     continue
+
                 # value in grid
                 actual_value = grid[pos_tested[1]][pos_tested[0]]
+
+                # if value is a new value 
                 if (actual_value != 0 and pos_tested not in pixel_found):
+                    # we add to pixel_found (passed throug hreference)
                     pixel_found.append(pos_tested)
                     pos_already_visited.append(pos_tested)
+                    # recursive call to check position
                     self.CreateNodeWithNeighbors(grid, pos_tested, pixel_found, pos_already_visited)
 
     def CreateNodeHorizontaly(self, grid):
@@ -76,8 +102,12 @@ class Graph:
     def CreateNodeVertically(self, grid):
         pass
 
+    # function to create the edges
+    # grid = input grid
     def CreateEdges(self, grid):
 
+        # 1 : dictionnary (key=>value) with key = position and value=node associated for this pixel position
+        # useful for the search 
         pixels_associated = {}
         for node in self.nodes:
             pixels = node.GetPixelPositions()
@@ -85,31 +115,42 @@ class Graph:
                 pixels_associated[pos] = node
 
 
-        i = 0
+        # foreach node
         for node in self.nodes:
-            i += 1
-            if i == 2:
-                print("")
+            # get all pixel for this node
             pixels = node.GetPixelPositions()
             for pos in pixels:
+
+                # we do 4 for: in for left, right, up, down
+                # we will check in all direction if there is a pixel, if yes, we check for the associated node (with pixels_associated)
+
+                # RIGHT DIRECTION
                 for x in range (pos[0], len(grid[0])):
                     pos_tested = (x, pos[1])
+                    # if there is a node 
                     if (grid[pos_tested[1]][pos_tested[0]] != 0 and pos_tested not in pixels):
                         if pos_tested in pixels_associated:
+                            # we associate current node to the node found
                             node.AddAssociatedNode(pixels_associated[pos_tested])
                             break 
+                
+                # LEFT DIRECTION
                 for x in range (pos[0], -1, -1):
                     pos_tested = (x, pos[1])
                     if (grid[pos_tested[1]][pos_tested[0]] != 0 and pos_tested not in pixels):
                         if pos_tested in pixels_associated:
                             node.AddAssociatedNode(pixels_associated[pos_tested])
                             break
+
+                # DOWN DIRECTION        
                 for y in range (pos[1], len(grid)):
                     pos_tested = (pos[0], y)
                     if (grid[pos_tested[1]][pos_tested[0]] != 0 and pos_tested not in pixels):
                         if pos_tested in pixels_associated:
                             node.AddAssociatedNode(pixels_associated[pos_tested])
                             break
+
+                # UP DIRECTION        
                 for y in range (pos[1], -1, -1):
                     pos_tested = (pos[0], y)
                     if (grid[pos_tested[1]][pos_tested[0]] != 0 and pos_tested not in pixels):
@@ -119,10 +160,12 @@ class Graph:
         print(self.nodes)
 
 
-    def build_graph_from_list(self, nodes):
+    # function to create the graphical graph
+    def BuildGraphFromList(self, nodes):
         G = nx.Graph()
         seen = set()
 
+        # foreach node, we add it to the nx graph type
         for node in nodes:
             if node not in seen:
                 G.add_node(node, color=node.color)
@@ -132,11 +175,14 @@ class Graph:
                 G.add_edge(node, neighbor)
         return G
 
+    # function to display graph
     def ShowGraph(self):
-        graph = self.build_graph_from_list(self.nodes)
+        # create graphical graph
+        graph = self.BuildGraphFromList(self.nodes)
         pos = nx.spring_layout(graph)
         node_colors = [data['color'] for _, data in graph.nodes(data=True)]
 
+        # draw graph
         nx.draw(graph, pos, with_labels=True, node_color=node_colors, edge_color="gray",
                 node_size=1000, font_size=12)
         plt.show()
